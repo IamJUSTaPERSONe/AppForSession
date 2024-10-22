527bd85f78724fbb8f2f8c2f8b1df56eb5626dfa
IamJUSTaPERSONe <168302104+IamJUSTaPERSONe@users.noreply.github.com>
2024-10-22 15:24:14 +0300

    Add files via upload

--- sing_in_out.py
+++ sing_in_out.py
@@ -3,7 +3,7 @@ import sqlite3
 
 
 def main(page: ft.Page):
-    page.title = 'Регистрация'
+    page.title = 'It_app'
     page.theme_mode = 'dark'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.window_width = 350
@@ -21,6 +21,7 @@ def main(page: ft.Page):
         )""")
         cursor.execute(f'INSERT INTO users VALUES(NULL, "{user_login.value}", "{user_password.value}")')
 
+        data_base.commit()
         data_base.close()
 
         user_login.value = ''
@@ -31,30 +32,81 @@ def main(page: ft.Page):
     def validate(event):
         if all([user_login.value, user_password.value]):
             button_reg.disabled = False
+            button_auth.disabled = False
         else:
             button_reg.disabled = True
+            button_auth.disabled = True
         page.update()
+
+    def user_auth(event):
+        data_base = sqlite3.connect('Base')
+
+        cursor = data_base.cursor()
+        cursor.execute(f'SELECT * FROM users WHERE login = "{user_login.value}" AND password = "{user_password.value}"')
+        if cursor.fetchone() != None:
+            user_login.value = ''
+            user_password.value = ''
+            button_auth.text = 'Авторизовано'
+            page.update()
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text('Неверно введены данные'))
+            page.snack_bar.open = True
+            page.update()
+
+        data_base.commit()
+        data_base.close()
+
+
 
     user_login = ft.TextField(label='Логин', width=200, on_change=validate)
     user_password = ft.TextField(label='Пароль', password=True, width=200, on_change=validate)
 
     button_reg = ft.OutlinedButton(text='Добавить', width=200, disabled=True, on_click=reg)
+    button_auth = ft.OutlinedButton(text='Авторизоваться', width=200, disabled=True, on_click=user_auth)
 
-    page.add(
-        ft.Row(
+    panel_registration = ft.Row(
             [
-                ft.Column(
-                    [
-                        ft.Text('Регистрация'),
-                        user_login,
-                        user_password,
-                        button_reg
-                    ]
-                )
-            ],
+                        ft.Column(
+                            [
+                                ft.Text('Регистрация'),
+                                user_login,
+                                user_password,
+                                button_reg
+                            ]
+                        )
+                    ],
             alignment=ft.MainAxisAlignment.CENTER
-        )
     )
+
+    panel_auth = ft.Row(
+            [
+                        ft.Column(
+                            [
+                                ft.Text('Авторизация'),
+                                user_login,
+                                user_password,
+                                button_auth
+                            ]
+                        )
+                    ],
+            alignment=ft.MainAxisAlignment.CENTER
+    )
+
+    def navigation(event):
+        index = page.navigation_bar.selected_index
+        page.clean()
+
+        if index == 0: page.add(panel_registration)
+        elif index == 1: page.add(panel_auth)
+
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER, label='Регистрация'),
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER_OUTLINED, label='Авторизация')
+        ], on_change=navigation
+    )
+
+    page.add(panel_registration)
 
 
 
